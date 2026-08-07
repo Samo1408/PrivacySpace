45070c0aaa7e1ce4c6a06dd88cc62561ddbd6dfa
Samo1408 <ab.0ss0mm@gmail.com>
2026-08-07 19:12:14 +0300

    fix: syntax error in SettingsAppHookImpl.kt at line 79

--- app/src/main/java/cn/geektang/privacyspace/hook/impl/SettingsAppHookImpl.kt
+++ app/src/main/java/cn/geektang/privacyspace/hook/impl/SettingsAppHookImpl.kt
@@ -7,7 +7,6 @@ import android.content.pm.*
 import android.os.Build
 import cn.geektang.privacyspace.hook.HookMain
 import cn.geektang.privacyspace.hook.Hooker
-import cn.geektang.privacyspace.util.ConfigHelper.getPackageName
 import cn.geektang.privacyspace.util.XLog
 import cn.geektang.privacyspace.util.tryLoadClass
 import de.robv.android.xposed.XC_MethodHook
@@ -19,7 +18,10 @@ object SettingsAppHookImpl : Hooker, XC_MethodHook() {
     override fun start(classLoader: ClassLoader) {
         val packageManagerClass = try {
             classLoader.tryLoadClass("android.content.pm.IPackageManager\$Stub\$Proxy")
-        } catch (e: ClassNotFoundException) { XLog.e(e, "SettingsAppHookImpl start failed."); return }
+        } catch (e: ClassNotFoundException) {
+            XLog.e(e, "SettingsAppHookImpl start failed.")
+            return
+        }
         for (method in packageManagerClass.declaredMethods) {
             when (method.name) {
                 "getInstalledPackages", "getInstalledApplications", "getInstalledModules", "queryIntentActivities" -> {
@@ -28,9 +30,19 @@ object SettingsAppHookImpl : Hooker, XC_MethodHook() {
                 else -> {}
             }
         }
-        XposedHelpers.findAndHookMethod(UsageStatsManager::class.java, "queryUsageStats",
-            Int::class.javaPrimitiveType, Long::class.javaPrimitiveType, Long::class.javaPrimitiveType, this)
-        XposedHelpers.findAndHookMethod(ActivityManager::class.java, "getRunningAppProcesses", this)
+        XposedHelpers.findAndHookMethod(
+            UsageStatsManager::class.java,
+            "queryUsageStats",
+            Int::class.javaPrimitiveType,
+            Long::class.javaPrimitiveType,
+            Long::class.javaPrimitiveType,
+            this
+        )
+        XposedHelpers.findAndHookMethod(
+            ActivityManager::class.java,
+            "getRunningAppProcesses",
+            this
+        )
     }
 
     override fun afterHookedMethod(param: MethodHookParam) {
@@ -40,14 +52,18 @@ object SettingsAppHookImpl : Hooker, XC_MethodHook() {
                 val result = param.result as ParceledListSlice<PackageInfo>
                 val iterator = result.list.iterator()
                 while (iterator.hasNext()) {
-                    if (hiddenAppList.contains(iterator.next().packageName)) iterator.remove()
+                    if (hiddenAppList.contains(iterator.next().packageName)) {
+                        iterator.remove()
+                    }
                 }
             }
             "getInstalledApplications" -> {
                 val result = param.result as ParceledListSlice<ApplicationInfo>
                 val iterator = result.list.iterator()
                 while (iterator.hasNext()) {
-                    if (hiddenAppList.contains(iterator.next().packageName)) iterator.remove()
+                    if (hiddenAppList.contains(iterator.next().packageName)) {
+                        iterator.remove()
+                    }
                 }
             }
             "getInstalledModules" -> {
@@ -55,7 +71,9 @@ object SettingsAppHookImpl : Hooker, XC_MethodHook() {
                     val result = param.result as MutableList<ModuleInfo>
                     val iterator = result.iterator()
                     while (iterator.hasNext()) {
-                        if (hiddenAppList.contains(iterator.next().packageName)) iterator.remove()
+                        if (hiddenAppList.contains(iterator.next().packageName)) {
+                            iterator.remove()
+                        }
                     }
                 }
             }
@@ -63,22 +81,34 @@ object SettingsAppHookImpl : Hooker, XC_MethodHook() {
                 val result = param.result as ParceledListSlice<ResolveInfo>
                 val iterator = result.list.iterator()
                 while (iterator.hasNext()) {
-                    if (hiddenAppList.contains(iterator.next().getPackageName())) iterator.remove()
+                    if (hiddenAppList.contains(iterator.next().getPackageName())) {
+                        iterator.remove()
+                    }
                 }
             }
             "queryUsageStats" -> {
                 val result = param.result as MutableList<UsageStats>
                 val iterator = result.iterator()
                 while (iterator.hasNext()) {
-                    if (hiddenAppList.contains(iterator.next().packageName)) iterator.remove()
+                    if (hiddenAppList.contains(iterator.next().packageName)) {
+                        iterator.remove()
+                    }
                 }
             }
             "getRunningAppProcesses" -> {
-                param.result = (param.result as? List<*>?)?.filter {
+                val result = param.result as? List<*> ?: return
+                param.result = result.filter {
                     it as ActivityManager.RunningAppProcessInfo
-                    !it.pkgList.any { pkg -> hiddenAppList.contains(pkg) }
+                    var shouldFilter = false
+                    it.pkgList.forEach { pkg ->
+                        if (hiddenAppList.contains(pkg)) {
+                            shouldFilter = true
+                        }
+                    }
+                    !shouldFilter
                 }
             }
+            else -> {}
         }
     }
 }
