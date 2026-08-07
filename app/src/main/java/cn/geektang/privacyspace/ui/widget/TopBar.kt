676f1a39f585f3374dc2940b27ed47de0e91a55d
Samo1408 <ab.0ss0mm@gmail.com>
2026-08-07 19:09:02 +0300

    fix: TopBar.kt + all UI insets files + LauncherScreen + ViewModel fixes

--- app/src/main/java/cn/geektang/privacyspace/ui/widget/TopBar.kt
+++ app/src/main/java/cn/geektang/privacyspace/ui/widget/TopBar.kt
@@ -3,8 +3,9 @@ package cn.geektang.privacyspace.ui.widget
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -22,6 +23,8 @@ import cn.geektang.privacyspace.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+fun Modifier.statusBarsPadding() = this.padding(WindowInsets.statusBars.asPaddingValues())
+
 @Composable
 fun TopBar(
     title: String,
@@ -30,198 +33,75 @@ fun TopBar(
     onNavigationIconClick: (() -> Unit)? = null
 ) {
     var navigationIcon: @Composable (() -> Unit)? = null
-    if (showNavigationIcon) {
-        navigationIcon = {
-            NavigationIcon(onNavigationIconClick)
-        }
-    }
-
-    Surface(
-        color = MaterialTheme.colors.primarySurface,
-        elevation = AppBarDefaults.TopAppBarElevation
-    ) {
-        val statusBarPadding = WindowInsets.statusBars.asPaddingValues()
-        Box(
-            modifier = Modifier
-                .then(
-                    Modifier.padding(
-                        top = statusBarPadding.calculateTopPadding(),
-                        start = statusBarPadding.calculateStartPadding(),
-                        end = statusBarPadding.calculateEndPadding()
-                    )
-                )
-        ) {
-            TopAppBar(
-                title = {
-                    Text(text = title)
-                },
-                actions = actions,
-                backgroundColor = Color.Transparent,
-                elevation = 0.dp,
-                navigationIcon = navigationIcon
-            )
+    if (showNavigationIcon) navigationIcon = { NavigationIcon(onNavigationIconClick) }
+    Surface(color = MaterialTheme.colors.primarySurface, elevation = AppBarDefaults.TopAppBarElevation) {
+        Box(Modifier.statusBarsPadding()) {
+            TopAppBar(title = { Text(text = title) }, actions = actions,
+                backgroundColor = Color.Transparent, elevation = 0.dp, navigationIcon = navigationIcon)
         }
     }
 }
 
 @Composable
 private fun NavigationIcon(onNavigationIconClick: (() -> Unit)?) {
-    IconButton(onClick = {
-        onNavigationIconClick?.invoke()
-    }) {
-        Icon(
-            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-            contentDescription = stringResource(R.string.back)
-        )
+    IconButton(onClick = { onNavigationIconClick?.invoke() }) {
+        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = stringResource(R.string.back))
     }
 }
 
 @Composable
-fun SearchTopBar(
-    title: String,
-    searchText: String,
-    onSearchTextChange: (String) -> Unit,
-    showMorePopupState: MutableState<Boolean>,
-    onNavigationIconClick: (() -> Unit)? = null
+fun SearchTopBar(title: String, searchText: String, onSearchTextChange: (String) -> Unit,
+    showMorePopupState: MutableState<Boolean>, onNavigationIconClick: (() -> Unit)? = null
 ) {
-    Surface(
-        color = MaterialTheme.colors.primarySurface,
-        elevation = AppBarDefaults.TopAppBarElevation
-    ) {
-        val statusBarPadding = WindowInsets.statusBars.asPaddingValues()
-        Box(
-            modifier = Modifier
-                .then(
-                    Modifier.padding(
-                        top = statusBarPadding.calculateTopPadding(),
-                        start = statusBarPadding.calculateStartPadding(),
-                        end = statusBarPadding.calculateEndPadding()
-                    )
-                )
-        ) {
-            SearchTopBarInner(
-                title = title,
-                searchText = searchText,
-                onSearchTextChange = onSearchTextChange,
-                showMorePopupState = showMorePopupState,
-                onNavigationIconClick = onNavigationIconClick
-            )
+    Surface(color = MaterialTheme.colors.primarySurface, elevation = AppBarDefaults.TopAppBarElevation) {
+        Box(Modifier.statusBarsPadding()) {
+            SearchTopBarInner(title, searchText, onSearchTextChange, showMorePopupState, onNavigationIconClick)
         }
     }
 }
 
 @Composable
-private fun SearchTopBarInner(
-    title: String,
-    searchText: String,
-    onSearchTextChange: (String) -> Unit,
-    showMorePopupState: MutableState<Boolean>,
-    onNavigationIconClick: (() -> Unit)?
+private fun SearchTopBarInner(title: String, searchText: String, onSearchTextChange: (String) -> Unit,
+    showMorePopupState: MutableState<Boolean>, onNavigationIconClick: (() -> Unit)?
 ) {
-    var showSearchBox by remember {
-        mutableStateOf(false)
-    }
+    var showSearchBox by remember { mutableStateOf(false) }
     val focusRequester = FocusRequester()
-    TopAppBar(
-        title = {
-            if (!showSearchBox) {
-                Text(text = title)
-            }
-        },
+    TopAppBar(title = { if (!showSearchBox) Text(text = title) },
         actions = {
-            if (showSearchBox) {
-                SearchBoxTextField(focusRequester, searchText, onSearchTextChange)
-            }
-
+            if (showSearchBox) SearchBoxTextField(focusRequester, searchText, onSearchTextChange)
             val scope = rememberCoroutineScope()
-            if (!showSearchBox) {
-                IconButton(onClick = {
-                    showSearchBox = true
-                    scope.launch {
-                        delay(100)
-                        focusRequester.smartRequestFocus()
-                    }
-                }) {
-                    Icon(
-                        imageVector = Icons.Filled.Search,
-                        contentDescription = stringResource(R.string.menu)
-                    )
-                }
+            if (!showSearchBox) IconButton(onClick = { showSearchBox = true; scope.launch { delay(100); focusRequester.smartRequestFocus() } }) {
+                Icon(Icons.Filled.Search, contentDescription = stringResource(R.string.menu))
             }
-
-            IconButton(onClick = {
-                showMorePopupState.value = true
-            }) {
-                Icon(
-                    imageVector = Icons.Filled.MoreVert,
-                    contentDescription = stringResource(R.string.menu)
-                )
+            IconButton(onClick = { showMorePopupState.value = true }) {
+                Icon(Icons.Filled.MoreVert, contentDescription = stringResource(R.string.menu))
             }
         },
         navigationIcon = {
             IconButton(onClick = {
-                if (showSearchBox) {
-                    if (searchText.isNotEmpty()) {
-                        onSearchTextChange("")
-                    }
-                    showSearchBox = false
-                } else {
-                    onNavigationIconClick?.invoke()
-                }
-            }) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = stringResource(R.string.back)
-                )
-            }
+                if (showSearchBox) { if (searchText.isNotEmpty()) onSearchTextChange(""); showSearchBox = false }
+                else onNavigationIconClick?.invoke()
+            }) { Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = stringResource(R.string.back)) }
         },
-        backgroundColor = Color.Transparent,
-        elevation = 0.dp,
-    )
+        backgroundColor = Color.Transparent, elevation = 0.dp)
 }
 
 @Composable
-private fun RowScope.SearchBoxTextField(
-    focusRequester: FocusRequester,
-    searchText: String,
-    onSearchTextChange: (String) -> Unit
-) {
+private fun RowScope.SearchBoxTextField(focusRequester: FocusRequester, searchText: String, onSearchTextChange: (String) -> Unit) {
     TextField(
-        modifier = Modifier
-            .focusRequester(focusRequester)
-            .weight(1f),
-        placeholder = {
-            Text(text = stringResource(R.string.search))
-        },
+        modifier = Modifier.focusRequester(focusRequester).weight(1f),
+        placeholder = { Text(text = stringResource(R.string.search)) },
         singleLine = true,
         colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            cursorColor = MaterialTheme.colors.secondary,
-            textColor = Color.White,
-            placeholderColor = Color(0xffcccccc)
-        ),
+            backgroundColor = Color.Transparent, focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent, cursorColor = MaterialTheme.colors.secondary,
+            textColor = Color.White, placeholderColor = Color(0xffcccccc)),
         trailingIcon = {
-            if (searchText.isNotEmpty()) {
-                IconButton(onClick = { onSearchTextChange("") }) {
-                    Icon(
-                        imageVector = Icons.Filled.Clear,
-                        tint = Color(0xccffffff),
-                        contentDescription = stringResource(R.string.clear)
-                    )
-                }
+            if (searchText.isNotEmpty()) IconButton(onClick = { onSearchTextChange("") }) {
+                Icon(Icons.Filled.Clear, tint = Color(0xccffffff), contentDescription = stringResource(R.string.clear))
             }
         },
-        value = searchText,
-        onValueChange = onSearchTextChange
-    )
+        value = searchText, onValueChange = onSearchTextChange)
 }
 
-private suspend fun FocusRequester.smartRequestFocus() {
-    try {
-        requestFocus()
-    } catch (e: Throwable) {
-        delay(100)
-    }
-}
+private suspend fun FocusRequester.smartRequestFocus() { try { requestFocus() } catch (_: Throwable) { delay(100) } }
